af670ceb83f656e16b4d29164039b08210df21b3
Shane Thomas <shane@devshane.com>
2014-11-16 23:18:17 -0500

    updates poles

--- Preech_ScrollFish.lua
+++ Preech_ScrollFish.lua
@@ -1,6 +1,10 @@
 INV_SLOT_MAIN_HAND = 16;
 
-POLES = { 45858 }; -- Nat's Lucky Fishing Pole
+POLES = {
+    6367,     -- Big Iron Fishing Pole
+    19022,    -- Nat Pagle's Extreme Angler FC-5000
+    45858,    -- Nat's Lucky Fishing Pole
+};
 
 local working = false;
 
