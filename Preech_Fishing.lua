81af9b208ead98cd72f8f46b48499fc25ad158e6
Shane Thomas <shane@devshane.com>
2014-11-17 18:26:55 -0500

    moves pole stuff to its own file

---
+++ Preech_Fishing.lua
@@ -0,0 +1,7 @@
+INV_SLOT_MAIN_HAND = 16;
+
+POLES = {
+    6367,     -- Big Iron Fishing Pole
+    19022,    -- Nat Pagle's Extreme Angler FC-5000
+    45858,    -- Nat's Lucky Fishing Pole
+};
--- Preech_ScrollFish.lua
+++ Preech_ScrollFish.lua
@@ -1,11 +1,3 @@
-INV_SLOT_MAIN_HAND = 16;
-
-POLES = {
-    6367,     -- Big Iron Fishing Pole
-    19022,    -- Nat Pagle's Extreme Angler FC-5000
-    45858,    -- Nat's Lucky Fishing Pole
-};
-
 local working = false;
 
 local function HexToRGBPerc(hex)
