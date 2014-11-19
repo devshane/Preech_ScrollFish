1b208c3699f36264dc0f81ece34294bc464951ab
Shane Thomas <shane@devshane.com>
2014-11-18 21:13:14 -0500

    adds more poles

--- Preech_Fishing.lua
+++ Preech_Fishing.lua
@@ -1,7 +1,10 @@
 INV_SLOT_MAIN_HAND = 16;
 
 POLES = {
+    6256,     -- Fishing Pole
+    6365,     -- Strong Fishing Pole
     6367,     -- Big Iron Fishing Pole
     19022,    -- Nat Pagle's Extreme Angler FC-5000
     45858,    -- Nat's Lucky Fishing Pole
+    118381,   -- Ephermeral Fishing Pole
 };
