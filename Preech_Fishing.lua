d0277b1b53281a721abcb5a4ecab28b6b19a58a0
Shane Thomas <shane@devshane.com>
2014-11-25 17:14:01 -0500

    adds dragon rod

--- Preech_Fishing.lua
+++ Preech_Fishing.lua
@@ -6,5 +6,6 @@ POLES = {
     6367,     -- Big Iron Fishing Pole
     19022,    -- Nat Pagle's Extreme Angler FC-5000
     45858,    -- Nat's Lucky Fishing Pole
+    84661,    -- Dragon Fishing Pole
     118381,   -- Ephermeral Fishing Pole
 };
