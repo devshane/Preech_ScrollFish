bb7c59ac64f9f1168d763a2a369e28f4820995a6
Shane Thomas <shane@devshane.com>
2015-01-04 10:06:21 -0500

    adds kalu'ak pole

--- Preech_Fishing.lua
+++ Preech_Fishing.lua
@@ -5,6 +5,7 @@ POLES = {
     6365,     -- Strong Fishing Pole
     6367,     -- Big Iron Fishing Pole
     19022,    -- Nat Pagle's Extreme Angler FC-5000
+    44050,    -- Mastercraft Kalu'ak Fishing Pole
     45858,    -- Nat's Lucky Fishing Pole
     84661,    -- Dragon Fishing Pole
     118381,   -- Ephermeral Fishing Pole
